18624ac57d91ad21f48c8d201c1f8eaf0ab9f5f9
Programando Ideas <programando.ideas123@gmail.com>
2020-06-13 13:25:48 -0500

    Correccion menor sobre AppSettings.

--- Apis/Ejercicio2.Api.Users/Helpers/AppSettings.cs
+++ Apis/Ejercicio2.Api.Users/Helpers/AppSettings.cs
@@ -7,7 +7,7 @@ namespace Ejercicio2.Api.Users.Helpers
 {
     public class AppSettings
     {
-        public string OriginCors { get; set; }
+        public string[] OriginCors { get; set; }
         public string Secret { get; set; }
         public string Issuer { get; set; }
         public string Audience { get; set; }
